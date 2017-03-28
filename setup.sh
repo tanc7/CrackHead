71cc9b4f4674362798d491869235777b8ab8c82a
tanc7 <tanc7@unlv.nevada.edu>
2017-03-28 15:01:13 -0700

    Standalone Copy of CrackHead
    This is my own repackaged copy of my other work, the Wifi Attack Autoloader Project.

---
+++ setup.sh
@@ -0,0 +1,7 @@
+chmod 777 CrackHead.py
+cp -r CrackHead.py /usr/local/bin
+mkdir /root/CrackHead
+cp -r ./ /root/CrackHead
+echo Installation Complete, type
+echo CrackHead.py
+echo In your terminal to launch CrackHead
